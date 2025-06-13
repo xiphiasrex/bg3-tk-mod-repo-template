341197deae615574ed7371489c0b4862c36e5c96
XiphiasRex <xiphiasrex@gmail.com>
2025-06-12 20:37:18 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # bg3-tk-mod-repo-template
+
+
 Git repo template for bg3 toolkit mod projects
+
+
+## Required Dependencies
+- Python runtime
+  - https://www.python.org/downloads/
+- Git
+  - https://git-scm.com/downloads
+  - There are other clients as well, I use https://desktop.github.com/download/
+- Junctions
+  - https://learn.microsoft.com/en-us/sysinternals/downloads/junction
+  - This is not an installer, but an executable.  Either place somewhere that is in your path
+    or make a note of the path to where this is kept
+
+
+## Usage
+1. Create a project in the Baldur's Gate 3 Toolkit
+2. Clone this repo to a non-protected directory (not in something like Program Files)
+   1. You can give a name to the local repo to reflect the specific mod
+3. Update properties at the top of `repo-setup.py`
+4. Run `python repo-setup.py`
---
+++ repo-setup.py
@@ -0,0 +1,67 @@
+import subprocess
+import sys
+from pathlib import Path
+
+
+"""
+    These properties should be adjusted before running the script!
+    
+    REPO_DIR: If you want to move around the relative path to the actual mod data.  Generally leave as-is
+    GAME_DATA_ROOT: Path to your BG3 Data folder
+    PATH_TO_JUNCTION_EXE: If junction.exe was added to your PATH variable, leave as-is.  Otherwise path to the exe
+    MOD_NAME_FULL: Full project name with UUID, ex. YourModNameHere_857aee99-5f79-fd6e-bf15-8c41e6863b1b
+    CREATE_NON_PROJECT_FILES_FOLDER: Creates a directory for toolkit related files like .blend, documents, etc. to store with mod
+"""
+REPO_DIR: str = "."
+GAME_DATA_ROOT: str = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Baldurs Gate 3\\Data"
+PATH_TO_JUNCTION_EXE: str = "junction.exe"
+MOD_NAME_FULL: str = "YourModNameHere_857aee99-5f79-fd6e-bf15-8c41e6863b1b"
+CREATE_NON_PROJECT_FILES_FOLDER: bool = True
+
+
+"""
+    *** Unless you know what you are doing don't modify below here!! ***
+"""
+def main():
+    # Set up new dir for mod if not already created
+    repo = Path(REPO_DIR)
+    mod_subfolder = repo / MOD_NAME_FULL
+    if mod_subfolder.exists():
+        sys.exit("Repo folder already exists for mod")
+    mod_subfolder.mkdir()
+
+    # Check that Game path is valid
+    game_data_path = Path(GAME_DATA_ROOT)
+    if not game_data_path.exists():
+        sys.exit(f'Game Data path invalid: [{GAME_DATA_ROOT}]')
+
+    # Check that mod project exists
+    if not (game_data_path / 'Mods' / MOD_NAME_FULL).exists():
+        sys.exit(f'No Mod Project {MOD_NAME_FULL} found')
+
+    # Create Sub-directories for project
+    (mod_subfolder / "Editor" / "Mods").mkdir(parents=True)
+    (mod_subfolder / "Generated" / "Public").mkdir(parents=True)
+    (mod_subfolder / "Mods").mkdir()
+    (mod_subfolder / "Projects").mkdir()
+    (mod_subfolder / "Public").mkdir()
+
+    # Create junctions to project folders
+    subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Editor\\Mods\\{MOD_NAME_FULL}"',
+                     shell=False, cwd=str(mod_subfolder/"Editor"/"Mods"))
+    subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Generated\\Public\\{MOD_NAME_FULL}"',
+                     shell=False, cwd=str(mod_subfolder/"Generated"/"Public"))
+    subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Mods\\{MOD_NAME_FULL}"',
+                     shell=False, cwd=str(mod_subfolder/"Mods"))
+    subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Projects\\{MOD_NAME_FULL}"',
+                     shell=False, cwd=str(mod_subfolder/"Projects"))
+    subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Public\\{MOD_NAME_FULL}"',
+                     shell=False, cwd=str(mod_subfolder/"Public"))
+
+    # Add folder for non-project files
+    if CREATE_NON_PROJECT_FILES_FOLDER:
+        (mod_subfolder / "Non-Project Files").mkdir()
+
+
+if __name__ == "__main__":
+    main()
