279b2cfee33ade61b113fad8dd178032b15a6784
XiphiasRex <xiphiasrex@gmail.com>
2025-06-12 20:56:22 -0500

    cleanup
    - changing error handling

--- README.md
+++ README.md
@@ -20,5 +20,6 @@ Git repo template for bg3 toolkit mod projects
 1. Create a project in the Baldur's Gate 3 Toolkit
 2. Clone this repo to a non-protected directory (not in something like Program Files)
    1. You can give a name to the local repo to reflect the specific mod
-3. Update properties at the top of `repo-setup.py`
+3. Update properties at the top of `repo-setup.py` and save
 4. Run `python repo-setup.py`
+5. Going forward you should be able to commit/push/pull/etc. like any other git repo
--- repo-setup.py
+++ repo-setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 """
     These properties should be adjusted before running the script!
-    
+
     REPO_DIR: If you want to move around the relative path to the actual mod data.  Generally leave as-is
     GAME_DATA_ROOT: Path to your BG3 Data folder
     PATH_TO_JUNCTION_EXE: If junction.exe was added to your PATH variable, leave as-is.  Otherwise path to the exe
@@ -23,21 +23,21 @@ CREATE_NON_PROJECT_FILES_FOLDER: bool = True
     *** Unless you know what you are doing don't modify below here!! ***
 """
 def main():
+    # Check that Game path is valid
+    game_data_path = Path(GAME_DATA_ROOT)
+    if not game_data_path.exists():
+        raise RuntimeError(f'Game Data path invalid: [{GAME_DATA_ROOT}]')
+
+    # Check that mod project exists
+    if not (game_data_path / 'Mods' / MOD_NAME_FULL).exists():
+        raise RuntimeError(f'Mod Project not found: [{MOD_NAME_FULL}]')
+
     # Set up new dir for mod if not already created
     repo = Path(REPO_DIR)
     mod_subfolder = repo / MOD_NAME_FULL
     if mod_subfolder.exists():
-        sys.exit("Repo folder already exists for mod")
+        raise RuntimeError(f'Local Mod Project folder already exists: [{MOD_NAME_FULL}]')
     mod_subfolder.mkdir()
-
-    # Check that Game path is valid
-    game_data_path = Path(GAME_DATA_ROOT)
-    if not game_data_path.exists():
-        sys.exit(f'Game Data path invalid: [{GAME_DATA_ROOT}]')
-
-    # Check that mod project exists
-    if not (game_data_path / 'Mods' / MOD_NAME_FULL).exists():
-        sys.exit(f'No Mod Project {MOD_NAME_FULL} found')
 
     # Create Sub-directories for project
     (mod_subfolder / "Editor" / "Mods").mkdir(parents=True)
@@ -48,15 +48,15 @@ def main():
 
     # Create junctions to project folders
     subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Editor\\Mods\\{MOD_NAME_FULL}"',
-                     shell=False, cwd=str(mod_subfolder/"Editor"/"Mods"))
+                     shell=False, cwd=str(mod_subfolder / "Editor" / "Mods"))
     subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Generated\\Public\\{MOD_NAME_FULL}"',
-                     shell=False, cwd=str(mod_subfolder/"Generated"/"Public"))
+                     shell=False, cwd=str(mod_subfolder / "Generated" / "Public"))
     subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Mods\\{MOD_NAME_FULL}"',
-                     shell=False, cwd=str(mod_subfolder/"Mods"))
+                     shell=False, cwd=str(mod_subfolder / "Mods"))
     subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Projects\\{MOD_NAME_FULL}"',
-                     shell=False, cwd=str(mod_subfolder/"Projects"))
+                     shell=False, cwd=str(mod_subfolder / "Projects"))
     subprocess.Popen(f'{PATH_TO_JUNCTION_EXE} ./{MOD_NAME_FULL} "{GAME_DATA_ROOT}\\Public\\{MOD_NAME_FULL}"',
-                     shell=False, cwd=str(mod_subfolder/"Public"))
+                     shell=False, cwd=str(mod_subfolder / "Public"))
 
     # Add folder for non-project files
     if CREATE_NON_PROJECT_FILES_FOLDER:
@@ -64,4 +64,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except RuntimeError as error:
+        print(error)
